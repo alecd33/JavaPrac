e4f92157b7f2f39449b519cf82b80a7e227779be
Alec Diaz <alecddiaz2@gmail.com>
2021-03-11 20:58:59 -0500

    lallaaland

--- src/testTheGit/Git.java
+++ src/testTheGit/Git.java
@@ -4,7 +4,7 @@ public class Git {
 
 	public static void main(String[] args) {
 		System.out.println("This is git");
-		System.out.println("We are learning git");
+		System.out.println("We git");
 	}
 
 }
